4e214fe81671665692767e19b017bfd081d3b5e8
Qbicz <avenger.v14@gmail.com>
2018-02-18 23:21:34 +0100

    print device info

--- trigger-image.py
+++ trigger-image.py
@@ -202,6 +202,41 @@ def acquire_images(cam, nodemap, nodemap_tldevice):
     return result
 
 
+def print_device_info(nodemap):
+    """
+    This function prints the device information of the camera from the transport
+    layer; please see NodeMapInfo example for more in-depth comments on printing
+    device information from the nodemap.
+
+    :param nodemap: Transport layer device nodemap.
+    :type nodemap: INodeMap
+    :returns: True if successful, False otherwise.
+    :rtype: bool
+    """
+
+    print "*** DEVICE INFORMATION ***\n"
+
+    try:
+        result = True
+        node_device_information = PySpin.CCategoryPtr(nodemap.GetNode("DeviceInformation"))
+
+        if PySpin.IsAvailable(node_device_information) and PySpin.IsReadable(node_device_information):
+            features = node_device_information.GetFeatures()
+            for feature in features:
+                node_feature = PySpin.CValuePtr(feature)
+                print "%s: %s" % (node_feature.GetName(),
+                                  node_feature.ToString() if PySpin.IsReadable(node_feature) else "Node not readable")
+
+        else:
+            print "Device control information not available."
+
+    except PySpin.SpinnakerException as ex:
+        print "Error: %s" % ex
+        return False
+
+    return result
+
+
 def run_single_camera(cam):
     """
     This function acts as the body of the example; please see NodeMapInfo example
